a3acb6ff4108092568df97aa9ae46f16b886bcd6
mohamedsayed1994 <35111471+mohamedsayed1994@users.noreply.github.com>
2020-02-20 23:17:48 +0200

    add vet list and controller

--- mypet-web/src/main/java/com/test/controllers/VetController.java
+++ mypet-web/src/main/java/com/test/controllers/VetController.java
@@ -1,13 +1,22 @@
 package com.test.controllers;
 
+import com.test.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String index() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
