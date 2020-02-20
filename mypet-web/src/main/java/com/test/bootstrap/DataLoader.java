34421180e072291d74271c784918413b70118b7d
mohamedsayed1994 <35111471+mohamedsayed1994@users.noreply.github.com>
2020-02-20 23:36:36 +0200

    edit on map service

--- mypet-web/src/main/java/com/test/bootstrap/DataLoader.java
+++ mypet-web/src/main/java/com/test/bootstrap/DataLoader.java
@@ -21,26 +21,22 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
         System.out.println("=========> Load Data");
         Owner o1 = new Owner();
-        o1.setId(1L);
         o1.setFirstName("Sayed");
         o1.setLastName("Omar");
         ownerService.save(o1);
 
         Owner o2 = new Owner();
-        o2.setId(2L);
         o2.setFirstName("Ahmed");
         o2.setLastName("Mustafa");
         ownerService.save(o2);
 
         System.out.println("=========> Owners Loaded");
         Vet v1 = new Vet();
-        v1.setId(1L);
         v1.setFirstName("Aly");
         v1.setLastName("Vet1");
         vetService.save(v1);
 
         Vet v2 = new Vet();
-        v2.setId(2L);
         v2.setFirstName("Jack");
         v2.setLastName("Vet");
         vetService.save(v2);
