1589942ceb31f696919637f980133bfa3eda12a5
mohamedsayed1994 <35111471+mohamedsayed1994@users.noreply.github.com>
2020-02-20 19:51:56 +0200

    Initial commit

---
+++ mypet-web/src/main/java/com/test/MypetV01Application.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetV01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypetV01Application.class, args);
+	}
+
+}
---
+++ mypet-web/src/test/java/com/test/MypetV01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypetV01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
