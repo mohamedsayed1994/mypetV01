20c0c3c54bd651fb8f8fd5a145305ce3096101c8
mohamedsayed1994 <35111471+mohamedsayed1994@users.noreply.github.com>
2020-02-21 23:47:15 +0200

    Create Visit Map Service. closes #31

---
+++ mypet-data/src/main/java/com/test/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.test.services;
+
+import com.test.model.Visit;
+
+public interface VisitService extends CrudeService<Visit, Long> {
+}
---
+++ mypet-data/src/main/java/com/test/services/map/VisitMapService.java
@@ -0,0 +1,45 @@
+package com.test.services.map;
+
+import com.test.model.Visit;
+import com.test.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    private final VisitService visitService;
+
+    public VisitMapService(VisitService visitService) {
+        this.visitService = visitService;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getId() == null ||
+                visit.getPet().getOwner() == null || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+}
