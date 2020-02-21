0d7706ebccf92d69344e3d1214e2cad5cbda9e1b
mohamedsayed1994 <35111471+mohamedsayed1994@users.noreply.github.com>
2020-02-21 22:55:29 +0200

    Create Spring Data jpa service for owners. closes #15

---
+++ mypet-data/src/main/java/com/test/services/springdatajpa/OwnerSDJpa.java
@@ -0,0 +1,58 @@
+package com.test.services.springdatajpa;
+
+import com.test.model.Owner;
+import com.test.repository.OwnerRepository;
+import com.test.repository.PetRepository;
+import com.test.repository.PetTypeRepository;
+import com.test.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springDataJpa")
+public class OwnerSDJpa implements OwnerService {
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OwnerSDJpa(OwnerRepository ownerRepository, PetRepository petRepository, PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return ownerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+}
