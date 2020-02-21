e067761f76e2391ed8527ba089279ac7004612a8
mohamedsayed1994 <35111471+mohamedsayed1994@users.noreply.github.com>
2020-02-21 15:10:22 +0200

    Enhance Owners with Contact info and pets on startup. closes #8

--- mypet-data/src/main/java/com/test/services/map/OwnerServiceMap.java
+++ mypet-data/src/main/java/com/test/services/map/OwnerServiceMap.java
@@ -1,13 +1,24 @@
 package com.test.services.map;
 
 import com.test.model.Owner;
+import com.test.model.Pet;
 import com.test.services.OwnerService;
+import com.test.services.PetService;
+import com.test.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+    private final PetService petService;
+    private final PetTypeService petTypeService;
+
+    public OwnerServiceMap(PetService petService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.petTypeService = petTypeService;
+    }
+
     @Override
     public Owner findById(Long id) {
         return super.findByID(id);
@@ -15,7 +26,27 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save( object);
+        if (object != null) {
+            if (object.getPets() != null) {
+                object.getPets().forEach(pet -> {
+                    if (pet.getPetType() != null) {
+                        if (pet.getPetType().getId() != null) {
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        } else {
+                            throw new RuntimeException("Pet Type is Required");
+                        }
+                    }
+                    if (pet.getId() == null) {
+                        Pet savePet = petService.save(pet);
+                        pet.setId(savePet.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        } else {
+            return null;
+        }
+
     }
 
     @Override
--- mypet-web/src/main/java/com/test/bootstrap/DataLoader.java
+++ mypet-web/src/main/java/com/test/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.test.bootstrap;
 
 import com.test.model.Owner;
+import com.test.model.Pet;
 import com.test.model.PetType;
 import com.test.model.Vet;
 import com.test.services.OwnerService;
@@ -8,6 +9,8 @@ import com.test.services.PetTypeService;
 import com.test.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -27,20 +30,40 @@ public class DataLoader implements CommandLineRunner {
 
         PetType dog = new PetType();
         dog.setName("dog");
-        petTypeService.save(dog);
+        PetType savedPetType = petTypeService.save(dog);
 
         PetType cat = new PetType();
         cat.setName("cat");
-        petTypeService.save(cat);
+        PetType savedPetType2 = petTypeService.save(cat);
 
         Owner o1 = new Owner();
         o1.setFirstName("Sayed");
         o1.setLastName("Omar");
+        o1.setAddress("Masr");
+        o1.setCity("Alex");
+        o1.setTelephone("123");
+
+        Pet pet1 = new Pet();
+        pet1.setName("pet1");
+        pet1.setPetType(savedPetType);
+        pet1.setOwner(o1);
+        pet1.setBirthDate(LocalDate.now());
+        o1.getPets().add(pet1);
         ownerService.save(o1);
 
         Owner o2 = new Owner();
         o2.setFirstName("Ahmed");
         o2.setLastName("Mustafa");
+        o2.setAddress("Cairo");
+        o2.setCity("EGY");
+        o2.setTelephone("12313123");
+
+        Pet pet2 = new Pet();
+        pet2.setName("pet2");
+        pet2.setPetType(savedPetType2);
+        pet2.setOwner(o2);
+        pet2.setBirthDate(LocalDate.now());
+        o2.getPets().add(pet2);
         ownerService.save(o2);
 
         System.out.println("=========> Owners Loaded");
