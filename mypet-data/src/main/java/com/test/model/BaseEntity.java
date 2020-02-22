e89968c02b11a3d33286d52f78b6e3c791e62f6c
mohamedsayed1994 <35111471+mohamedsayed1994@users.noreply.github.com>
2020-02-22 10:25:12 +0200

    add lombok project

--- mypet-data/src/main/java/com/test/model/BaseEntity.java
+++ mypet-data/src/main/java/com/test/model/BaseEntity.java
@@ -6,6 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+
 @MappedSuperclass
 public class BaseEntity implements Serializable {
     @Id
